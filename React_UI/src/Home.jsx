f921aa9c2fe4c27e8fde8c6cffd33e4aff53ddd6
dh0rwwit <dh0rwwit@gmail.com>
2025-09-03 17:45:21 +0900

    Home화면 수정

---
+++ React_UI/src/Home.jsx
@@ -0,0 +1,21 @@
+﻿import "./Home.css";
+
+export default function Home() {
+    return (
+        <div className="home">
+            <h1>Main Widget</h1>
+
+            {/* 하단 고정 버튼바 */}
+            <div className="bottom-bar">
+                <button>배당가정</button>
+                <button>배당내역</button>
+                <button>종목별순입금매수액</button>
+                <button>종목별평가</button>
+                <button>손익합</button>
+            </div>
+
+        </div>
+    );
+}
+
+
--- React_UI/src/main.jsx
+++ React_UI/src/main.jsx
@@ -2,14 +2,16 @@ import { StrictMode } from 'react'
 //import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom' // npm install react-router-dom
 import TableRowAdd from './tableRowAdd'
-import './index.css'
+//import './index.css'
 import App from './App.jsx'
 import ReactDOM from 'react-dom/client'
+
+import Home from "./Home.jsx"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
         <Routes>
-            <Route path="/" element={<App />} />
+            <Route path="/" element={<Home />} /> {/*맨 처음 보게 될 화면 App에서 Home으로 수정수정*/}
             <Route path="/add-row" element={<TableRowAdd />} />
             
                 
