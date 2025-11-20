0eb9e36bc0b5e4eecefbe0801fb40b37a041aeee
dh0rwwit <dh0rwwit@gmail.com>
2025-11-20 17:53:46 +0900

    Onion에서 하던 코드 이식 중

---
+++ ASP.NETcoreMVC/Dynamic/DynSelect.cs
@@ -0,0 +1,13 @@
+﻿namespace ASP.NETcoreMVC.DynSelect
+{
+    public record DynSelect
+    {
+        public string Table { get; init; } = "";
+        public string Schema { get; init; } = "pb";
+        public int Skip { get; init; }
+        public int Take { get; init; } = 100;
+        public string? WhereJson { get; init; } // {"col":{"op":"=","value":123}} , {"col":{"op":"ilike","value":"%abc%"}}
+        public string[]? Columns { get; init; }  // null이면 전체
+        public string? OrderBy { get; init; }    // 안전을 위해 아래에서 검증
+    }
+}
